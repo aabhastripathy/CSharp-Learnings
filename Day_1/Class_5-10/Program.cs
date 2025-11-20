b63a1487f2e5bbc542949f3c39f822db568223da
Aabhas <aabhastripathi2002@gmail.com>
2025-11-20 08:44:41 +0530

    Add project files.

---
+++ Day_1/Class_5-10/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Class_5_10
+{
+    public class Class1
+    {
+        private int i;//field
+        public int I//property
+        {
+            set
+            {
+                if (value <= 100)
+                {
+                    i = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid value");
+                }
+            }
+            get
+            {
+                return i;
+            }
+
+        }
+        public int J { get; set; } //auto property
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Class1 obj = new Class1();
+            obj.I = 100;
+            obj.J = 125;
+            Console.WriteLine(obj.I);
+            Console.WriteLine(obj.J);
+
+            //after this we move to constructors
+        }
+    }
+
+}
---
+++ Day_1/Task_04-10/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Task_04_10
+{
+
+    public class Employee 
+    {
+        private string name;
+        public string Name 
+        {
+            set {
+                if (value == "")
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                else {
+                    name = value;
+                }
+            }
+            get 
+            {
+                return name;
+            }
+        }
+        private int empno;
+        public int Empno 
+        {
+            set {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                else {
+                    empno = value;
+                }
+            }
+            get 
+            {
+                return empno;
+            }
+        }
+        private decimal basic;
+        public decimal Basic 
+        {
+            set {
+                if (value <= 10000 && value > 1000000) {
+                    Console.WriteLine("Invalid Input");
+                }
+                else {
+                    basic = value;
+                }
+            }
+            get 
+            {
+                return basic;
+            }
+        }
+        private short deptno;
+        public short Deptno 
+        {
+            set {
+                if (value <= 0) {
+                    Console.WriteLine("Invalid Input");
+                }
+                else
+                {
+                    deptno = value;
+                }
+            }
+            get 
+            {
+                return deptno;
+            }
+        }
+        public decimal GetNetSalary() 
+        {
+            return basic + 1000 - 150;
+        }
+        
+    }
+    
+    
+    
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee obj = new Employee();
+            obj.Name = "Aabhas";
+            obj.Empno = 1;
+            obj.Basic = 20000;
+            obj.Deptno = 2;
+            Console.WriteLine(obj.Name);
+            Console.WriteLine(obj.Empno);
+            Console.WriteLine(obj.Basic);
+            Console.WriteLine(obj.Deptno);
+            Console.WriteLine(obj.GetNetSalary());
+        }
+    }
+}
