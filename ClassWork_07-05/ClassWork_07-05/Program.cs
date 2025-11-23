a6c1bc049ca6cad08239b6c8bca1ba9d01b4a5e5
Aabhas <aabhastripathi2002@gmail.com>
2025-11-23 20:07:40 +0530

    day04

---
+++ ClassWork_07-05/ClassWork_07-05/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Struct_07
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Mypoint z = new Mypoint();
+            Console.WriteLine(z.A);
+            Console.WriteLine(z.B);
+            Console.WriteLine(z.X);
+            //Console.WriteLine(z.b);
+
+
+
+
+        }
+    }
+    public struct Mypoint 
+    {
+        public int A { get; set; }
+        public int X;
+        private int b;
+        public int B 
+        {
+            get 
+            {
+                return b;
+            }
+            set 
+            {
+                b = value;
+            }
+        }
+        public Mypoint() 
+        {
+            this.A = 1;
+            this.B = 2;
+            this.X = 3;
+            this.b = 40;
+        }
+    }
+}
