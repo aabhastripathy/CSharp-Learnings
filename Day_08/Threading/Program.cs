7f036c9fc7c040326c93aba3c1dcc8b27dc05ff2
Aabhas <aabhastripathi2002@gmail.com>
2025-11-27 08:39:54 +0530

    Day 8

---
+++ Day_08/Threading/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoResetEvent ar = new AutoResetEvent(false);
+            AutoResetEvent ar2 = new AutoResetEvent(false);
+            Thread t1 = new Thread(delegate ()
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    Console.WriteLine($"Function1 {i}");
+                    if (i % 50 == 0)
+                    {
+                        Console.WriteLine("Waiting");
+                        ar.WaitOne();
+                    }
+                }
+            });
+            
+            Thread t2 = new Thread(delegate () {
+                for (int i = 0; i < 200; i++)
+                {
+                    Console.WriteLine($"Function2 {i}");
+                    if (i % 100 == 0)
+                    {
+                        Console.WriteLine("Waiting");
+                        ar2.WaitOne();
+                    }
+                }
+            });
+            
+            for (int i = 0; i < 200; i++) {
+                Console.WriteLine($"Main {i}");
+            }
+            t1.Start();
+            Console.ReadLine();
+            ar.Set();
+            Console.ReadLine();
+            ar.Set();
+            Console.ReadLine();
+            ar.Set();
+            Console.ReadLine();
+            ar.Set();
+            t2.Start();
+            Console.ReadLine();
+            ar2.Set();
+            Console.ReadLine();
+            ar2.Set();
+
+        }
+        
+        static void function2()
+        {
+            AutoResetEvent ar = new AutoResetEvent(false);
+            
+        }
+    }
+}
---
+++ Day_08/ThreadingWithMultipalObjectsPassed/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ThreadingExamples2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(Func1));
+            string[] arr = new string[2];
+            arr[0] = "a";
+            arr[1] = "b";
+            t1.Start(arr);
+            //t1.Start("passed value");
+
+            Thread t2 = new Thread(Func2);
+            Class1 obj = new Class1 { P1 = 1, P2 = "aaa" };
+            t2.Start(obj);
+            //t2.Start(1234);
+        }
+
+        //problem statement - pass more than one value to func
+        //1. create a class/struct with the properties to be passed.
+        //   create an object , and pass the object
+        //2. create an array/collection. 
+        //3. create a local func/anon method/lambda.
+        //local variables can be directly accessed, no need to pass them.
+        static void Func1(object o)
+        {
+            string[] arr = (string[])o;
+            Console.WriteLine("First : " + arr[0]);
+            Console.WriteLine("First : " + arr[1]);
+
+            //Console.WriteLine("First : " + o);
+        }
+        static void Func2(object o)
+        {
+            Class1 obj = (Class1)o;
+            Console.WriteLine("Second : " + obj.P1);
+            Console.WriteLine("Second : " + obj.P2);
+
+            //Console.WriteLine("Second : " + o);
+        }
+    }
+
+    public class Class1
+    {
+        public int P1 { get; set; }
+        public string P2 { get; set; }
+
+    }
+}
